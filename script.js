be2d027824ff985e0a4a0300f9a7e070dc8d084f
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-01-15 09:18:53 +0200

    add css

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //QUERY SELECTORS
+let buttons = document.querySelector(".buttons")
 
 
 //EVENT LISTENERS
+buttons.addEventListener("click", () => {
+    console.log(buttons.value)
+})
 
 
 //FUNCTIONS
