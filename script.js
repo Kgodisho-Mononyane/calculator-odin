1c73aff3eca7ac013520ff2a86ed1f7ef9418db3
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-01-26 09:13:14 +0200

    remove big buttons section

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //QUERY SELECTORS
-let buttons = document.querySelector(".buttons")
+
 
 
 //EVENT LISTENERS
-buttons.addEventListener("click", () => {
-    console.log(buttons.value)
-})
+
 
 
 //FUNCTIONS
