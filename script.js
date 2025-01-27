6f9d5bad3a606b54e196d2e9dda328ad6648fb41
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-01-27 08:18:38 +0200

    add query selectors and event listeners for operator buttons

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 //QUERY SELECTORS
-buttons = document.querySelector(".buttons")
+clearBtn = document.querySelector("#clear-btn");
+bracketsBtn = document.querySelector("#brackets");
+percentageBtn = document.querySelector("#percentage");
+divisionBtn = document.querySelector("#division");
+multiplicationBtn = document.querySelector("#multiplication")
+subtractionBtn = document.querySelector("#subtraction");
+additionBtn = document.querySelector("#addition");
+pointBtn = document.querySelector("#point");
+deleteBtn = document.querySelector("#delete");
+equalsBtn = document.querySelector("#equals");
 
 
 //EVENT LISTENERS
-button.addEventListener("click", () => {
-    alert(buttons.value)
+clearBtn.addEventListener("click", () => {
+    clearBtn.style.backgroundColor = "red";
 })
 
+bracketsBtn.addEventListener("click", () => {
+    bracketsBtn.style.backgroundColor = "red";
+})
 
+percentageBtn.addEventListener("click", () => {
+    percentageBtn.style.backgroundColor = "red";
+})
+
+divisionBtn.addEventListener("click", () => {
+    divisionBtn.style.backgroundColor = "red";
+})
+
+multiplicationBtn.addEventListener("click", () => {
+    multiplicationBtn.style.backgroundColor = "red";
+})
+
+subtractionBtn.addEventListener("click", () => {
+    subtractionBtn.style.backgroundColor = "red";
+})
+
+additionBtn.addEventListener("click", () => {
+    additionBtn.style.backgroundColor = "red";
+})
+
+pointBtn.addEventListener("click", () => {
+    pointBtn.style.backgroundColor = "red";
+})
+
+deleteBtn.addEventListener("click", () => {
+    deleteBtn.style.backgroundColor = "red";
+})
+
+equalsBtn.addEventListener("click", () => {
+    equalsBtn.style.backgroundColor = "red";
+})
 
 //FUNCTIONS
