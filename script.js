8f589fa57c9d3c9304c7b63760f5664f9c9894dd
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-10-24 17:36:36 +0200

    remove redundant code from operator function

--- script.js
+++ script.js
@@ -70,19 +70,7 @@ function operate(num1, operator, num2) {
     }
     
     
-    if (operator == "add") {
-        return add(num1,num2);
-    } else if (operator == "subtract") {
-        return subtract(num1, num2);
-    } else if (operator == "multiply") {
-        return multiply(num1, num2);
-    } else if (operator == "divide") {
-        return divide(num1, num2);
-    } else if (operator == "remainder") {
-        return remainder(num1, num2);
-    } else {
-        return null;
-    }   
+       
 }
 
 
