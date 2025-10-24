a6e7e007dabe4fff8aba549054a98e9cee3ad4d8
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-10-24 09:22:18 +0200

    add operator conditions to operator function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//SET display
-//SET Buttons
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
 /*
 FOR all buttons
     button addEventListener
-        IF the button is clear THEN
+        IF button is clear THEN
             clear display
             clear out operate function
-        ELSEIF the buttons is backspace THEN
+        ELSEIF buttons is backspace THEN
             remove the last element from display
             remove last element from operate function
         ELSEIF operator THEN
@@ -20,29 +20,8 @@ FOR all buttons
             append value to display
             add value to operate function
         ENDIF
-
-FUNCTION add
-FUNCTION subtract
-FUNCTION multiply
-FUNCTION divide
-FUNCTION remainder
-FUNCTION clear
-FUNCTION backspace
-FUNCTION operate
-FUNCTION
-
-
-
-
-
-
-        */
-
-
-
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
-
+*/
+/*
 buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
         if (button.id == "clear-btn") {
@@ -53,9 +32,9 @@ buttons.forEach((button) => {
         console.log(`${button.id} clicked`)
     })
 })
+*/
 
 //FUNCTIONALITY
-
 function add(a, b) {
     return a + b;
 }
@@ -69,7 +48,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b == 0) {
+        return null;
+    } else {
+        return a / b;
+    }
 }
 
 function remainder(a, b) {
@@ -78,13 +61,38 @@ function remainder(a, b) {
 
 function clear() {
     display.value = "";
-    //clear all data and content
+    //remove operate function content 
 }
 
 function backspace() {
-    //remove one element one by one
+    //remove last display element
+    //remove last function element
 }
 
 function operate(num1, operator, num2) {
-    add(num1, num2)
+     // take in num1, num2 and operator
+    // IF num1, num2 or operator is missing THEN
+    //     return Null
+
+    if (operator == "add") {
+        return add(num1,num2);
+    } else if (operator == "subtract") {
+        return subtract(num1, num2);
+    } else if (operator == "multiply") {
+        return multiply(num1, num2);
+    } else if (operator == "divide") {
+        return divide(num1, num2);
+    } else if (operator == "remainder") {
+        return remainder(num1, num2);
+    } else {
+        return null;
+    }   
 }
+
+
+
+
+
+
+
+
